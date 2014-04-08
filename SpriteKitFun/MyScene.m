b31c5b9be2536452c7a4cef2705c2e1f98d22229
Aaron Baker <aaron.baker@imoutdoors.com>
2014-04-07 21:42:49 -0400

    Added Colors and textures

--- SpriteKitFun/MyScene.m
+++ SpriteKitFun/MyScene.m
@@ -9,6 +9,10 @@
 #import "MyScene.h"
 @import CoreMotion;
 
+@interface MyScene()
+@property CMMotionManager *motionManager;
+@end
+
 @implementation MyScene
 
 -(id)initWithSize:(CGSize)size {    
@@ -17,13 +21,15 @@
         
         SKNode *wall = [SKNode node];
         
+        self.physicsWorld.gravity = CGVectorMake(4.0, 9.8);
+        
         wall.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
         
         [self addChild:wall];
         
-        CMMotionManager *motionManager = [[CMMotionManager alloc] init];
+        self.motionManager = [[CMMotionManager alloc] init];
         
-        [motionManager startAccelerometerUpdatesToQueue:[[NSOperationQueue alloc] init] withHandler:^(CMAccelerometerData *data, NSError *error){
+        [self.motionManager startAccelerometerUpdatesToQueue:[[NSOperationQueue alloc] init] withHandler:^(CMAccelerometerData *data, NSError *error){
             
             self.physicsWorld.gravity = CGVectorMake(data.acceleration.x * 10, data.acceleration.y * 10);
             
@@ -44,10 +50,37 @@
         
         CGPoint location = [touch locationInNode:self];
         
-        SKSpriteNode *box = [SKSpriteNode
-                             spriteNodeWithColor:[UIColor redColor]
-                             size:CGSizeMake(40.0, 40.0)];
+        UIColor* red = [UIColor colorWithRed:0xFF/255.0f
+                                       green:0x41/255.0f
+                                        blue:0x36/255.0f alpha:1];
         
+        UIColor* yellow = [UIColor colorWithRed:0xFF/255.0f
+                                          green:0xDC/255.0f
+                                           blue:0x00/255.0f alpha:1];
+        
+        UIColor* fuchsia = [UIColor colorWithRed:0xF0/255.0f
+                                           green:0x12/255.0f
+                                            blue:0xBE/255.0f alpha:1];
+        
+        UIColor* blue = [UIColor colorWithRed:0x00/255.0f
+                                        green:0x74/255.0f
+                                         blue:0xD9/255.0f alpha:1];
+        
+        UIColor* lime = [UIColor colorWithRed:0x01/255.0f
+                                        green:0xFF/255.0f
+                                         blue:0x70/255.0f alpha:1];
+        
+        NSArray *colors = @[red,yellow,fuchsia,blue,lime];
+        
+        NSUInteger randomIndex = arc4random() % [colors count];
+
+        UIColor *randomColor = [colors objectAtIndex:randomIndex];
+        
+        SKSpriteNode *box = [SKSpriteNode
+                             spriteNodeWithColor:randomColor
+                             size:CGSizeMake(40.0, 40.0)];
+        box.texture = [SKTexture textureWithImageNamed:@"box"];
+        box.colorBlendFactor = 1.0;
         
         box.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:box.size];
         
