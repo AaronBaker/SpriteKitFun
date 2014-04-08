c4a37d91bc5dfdf94a9e0161789a06a8bdc6a735
Aaron Baker <aaron.baker@imoutdoors.com>
2014-04-07 21:06:25 -0400

    Initial Commit

---
+++ SpriteKitFun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitFun
+//
+//  Created by Aaron Baker on 4/7/14.
+//  Copyright (c) 2014 Aaron Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitFun/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpriteKitFun
+//
+
+//  Copyright (c) 2014 Aaron Baker. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
